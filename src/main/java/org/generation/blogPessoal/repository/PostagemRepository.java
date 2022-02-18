2f065ffa2db9dea3f0060bd5a0eb40047f955e83
Jean Lucas <jeanlucas588@outlook.com>
2022-02-18 15:00:30 -0300

    @JeanLRC[Task CREATE PostagemController]

--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,7 +1,7 @@
 package org.generation.blogPessoal.repository;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +11,4 @@ import org.springframework.stereotype.Repository;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
-	//public Optional<Postagem> findById(Long id);
 }
